5fe47523c264d05c22abbe5ca7a15938a83559f7
dhanushka365 <technohub10000@gmail.com>
2023-02-04 14:53:10 +0530

    initlal commit

--- tools/image_face_detect_vedio.py
+++ tools/image_face_detect_vedio.py
@@ -8,11 +8,12 @@ eye_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_eye.xml')
 while True:
     ret, frame = cap.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3 ,minNeighbors=5)
     print(len(faces))
 
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+        cv.putText(frame,'Face',(x, y), font, 1,(255,0,0),3)
         eye_gray = gray[y:y+h, x:x+w]
         eye_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(eye_gray)
