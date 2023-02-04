3a565c3e3032cc62ee495df46715f01405d5ff78
dhanushka365 <technohub10000@gmail.com>
2023-02-04 16:03:15 +0530

    smile detection added

--- tools/image_face_detect_vedio.py
+++ tools/image_face_detect_vedio.py
@@ -5,6 +5,7 @@ cap = cv.VideoCapture(0)
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_eye.xml')
 eye_glass_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_eye_tree_eyeglasses.xml')
+smile_cascade = cv.CascadeClassifier(cv.data.haarcascades +'haarcascade_smile.xml')
 
 while True:
     ret, frame = cap.read()
@@ -26,6 +27,10 @@ while True:
         for (ex, ey, ew, eh) in eyes_glass:
             cv.rectangle(eye_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
             cv.putText(frame,'Eye',(x + ex,y + ey), 1, 1, (0, 255, 0), 1)
+
+        Smile_rect = smile_cascade.detectMultiScale(gray, scaleFactor=1.8, minNeighbors=30 )
+        for (x, y, w, h) in Smile_rect:
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 150), thickness=2)   
     cv.putText(frame,'Number of Faces :' + str(len(faces)),(40, 40), font, 1,(255,0,0),2)        
     cv.imshow('frame', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
